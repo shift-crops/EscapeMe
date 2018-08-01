fcfdc3035cedf5d5ef05c897d13724220ec75db2
ShiftCrops <simiyu@shift-crops.net>
2018-08-02 03:20:57 +0900

    brk/mmap/mprotect/munmap

--- misc/brk.c
+++ misc/brk.c
@@ -1,9 +1,39 @@
 #include <stdint.h>
+#include <stddef.h>
+
+static void *__curbrk;
 
 int brk(void *addr){
+	int res;
 
+	asm volatile (
+		"mov rax, 0x0b\r\n"
+		"syscall\r\n"
+		"mov %0, eax"
+	: "=r"(res) :: "rax");
+
+	if(res == 0)
+		__curbrk = addr;
+	return res;
 }
 
 void *sbrk(intptr_t increment){
+	void *oldbrk;
 
+	if (__curbrk == NULL && brk(0) < 0)
+		return (void *) -1;
+
+	if (increment == 0)
+		return __curbrk;
+
+	oldbrk = __curbrk;
+	if (increment > 0
+			? ((uintptr_t) oldbrk + (uintptr_t) increment < (uintptr_t) oldbrk)
+			: ((uintptr_t) oldbrk < (uintptr_t) -increment))
+		return (void *) -1;
+
+	if (brk(oldbrk + increment) < 0)
+		return (void *) -1;
+
+	return oldbrk;
 }
--- misc/mmap.c
+++ misc/mmap.c
@@ -2,9 +2,24 @@
 #include <stdio.h>
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset){
+	asm volatile (
+		"mov r10, rcx\r\n"
+		"mov rax, 0x09\r\n"
+		"syscall"
+		);
 
 }
 
-int munmap(void *addr, size_t length){
+int mprotect(void *addr, size_t len, int prot){
+	asm volatile (
+		"mov rax, 0x0a\r\n"
+		"syscall"
+		);
+}
 
+int munmap(void *addr, size_t length){
+	asm volatile (
+		"mov rax, 0x0b\r\n"
+		"syscall"
+		);
 }
